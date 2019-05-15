71b032d644fa0dbfbdb2903d2fd503463d8bbbbd
github username <github email address>
2019-05-15 15:48:03 -0700

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,66 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  constructor(){
+    super()
+    this.state = {
+      money: 100,
+      currentIndex: 0,
+      sushis: [],
+      eatenSushis: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+
+      this.setState({
+        sushis: data
+      })
+    })
+  }
+
+  nextFourSushis = () => {
+    this.setState({
+      currentIndex: this.state.currentIndex + 4
+    })
+  }
+
+  isSushiEaten = (sushi) => {
+    return this.state.eatenSushis.includes(sushi)
+  }
+
+  eatSushi = (sushi) => {
+
+    if(this.isSushiEaten(sushi)) return
+    if(this.state.money - sushi.price < 0) return
+      this.setState({
+        eatenSushis: [...this.state.eatenSushis, sushi],
+        money: [this.state.money - sushi.price]
+      })
+
+  }
+
   render() {
+
+
+
+const {money, currentIndex, sushis, eatenSushis} = this.state
+
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushis={sushis.slice(currentIndex, currentIndex + 4)}
+          nextFourSushis={this.nextFourSushis}
+          isSushiEaten={this.isSushiEaten}
+          eatSushi={this.eatSushi}
+          />
+        <Table
+          money={money}
+          eatenSushis={eatenSushis}
+          />
       </div>
     );
   }
